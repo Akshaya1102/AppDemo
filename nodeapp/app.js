1971c998f64abe4f54f1e4e5e1997a0f572b68ed
Akshaya1102 <akshaya.ganesan@pilani.bits-pilani.ac.in>
2023-09-17 09:36:14 +0530

    app_with_mongodb

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
@@ -24,8 +23,8 @@ mongoose.connect(dbConfig.url, {
 //mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 //CORS headers
@@ -40,26 +39,24 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
-  setTimeout(() => {
-    next();
-  },10000);
-    
+   next();   
 });
 
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-app.use("/", (req, res, next) => {
+
+app.get("/", (req, res, next) => {
   res.status(200).json({
   message: "hi"
 });
 });
-
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
+
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
